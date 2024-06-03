7c41c227885425e87fd74fed2e973d25cf0e765e
RobledoJohn <robledoJohn@hotmail.com>
2024-06-02 19:05:29 -0500

    colocando ruta raiz con vista de laravel

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,14 @@
 <?php
 
-use App\Http\Controllers\api\authController;
 use Illuminate\Support\Facades\Route;
 
-/* ruta de la pagina de inicio que llama la vista welcome.blade.php
 Route::get('/', function () {
     return view('welcome');
 });
-*/
 
+/*
 //ruta para pagina de incio que muestra un mensaje
 Route::get('/', function () {
     return "CRUD Iniciado";
 });
-
-Route::post('/login', [authController::class, 'login']);
+*/
