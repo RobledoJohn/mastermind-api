117d4152632e0f366489e0d35af47df899b1e85b
RobledoJohn <robledoJohn@hotmail.com>
2024-06-04 16:54:51 -0500

    configurando vercel ok

--- app/Http/Controllers/api/empresaController.php
+++ app/Http/Controllers/api/empresaController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
-use App\Models\empresa;
+use App\Models\Empresa;
 
 class empresaController extends Controller
 {
--- app/Http/Controllers/api/ordenesController.php
+++ app/Http/Controllers/api/ordenesController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Ingreso;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+
+use App\Models\Ingreso;
 
 class ordenesController extends Controller
 {
