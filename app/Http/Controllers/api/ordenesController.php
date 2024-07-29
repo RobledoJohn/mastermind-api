809729bea7f168c211b5cdad5673c05fdde912ea
RobledoJohn <robledoJohn@hotmail.com>
2024-07-29 12:48:39 -0500

    api ingresos fix json

--- app/Http/Controllers/api/ordenesController.php
+++ app/Http/Controllers/api/ordenesController.php
@@ -4,29 +4,34 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Empresa;
-use App\Models\Ingreso;
-use App\Models\Tecnico;
-use Illuminate\Http\Request;
+
+use function Laravel\Prompts\select;
 
 class ordenesController extends Controller
 {
-    public function read(Request $request){
+    public function read($requestId){
 
         //tomamos el id de la empresa que se envia por query
-        $empresaId = $request->id;
-
+        $idEmpresa = $requestId;
+        
         /*Se verifica que el id sea enviado como parametro */
-        if ($empresaId == null) {
+        if ($idEmpresa == null) {
             $data = [
                 'mensaje' => 'No se envio id de empresa',
                 'status' => 404
             ];
             return response()->json($data, 404);
         }
+        
+        $ordenes = Empresa::where('empresas.id', $idEmpresa)
+        ->join('tecnicos', 'empresas.id', '=', 'tecnicos.id_empresa')
+        ->join('ingresos', 'tecnicos.id', '=', 'ingresos.id_tecnico')
+        ->select('ingresos.*', 'tecnicos.nombre as tecnico')
+        ->get();
 
-        //BUSCAR POR ID DE EMPRESA, Y DEVOLVER TODAS LAS ORDENES DE ESE ID
-        $ordenes = Empresa::find($empresaId)->tecnicos->get(0)->ingresos;
-
+        return response()->json($ordenes, 200);   
+        
+        
         if ($ordenes->isEmpty()) {
             $data = [
                 'mensaje' => 'No se encontraron ordenes',
@@ -35,6 +40,7 @@ class ordenesController extends Controller
             return response()->json($data, 404);
         } else {
             return response()->json($ordenes, 200);
-        }
+        }    
+
     }    
 }
